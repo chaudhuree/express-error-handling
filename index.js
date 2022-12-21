ebcc3a10bdb14dcf8173921612a78a8e708ea631
chaudhuree <chaudhuree00@gmail.com>
2022-12-21 22:47:05 +0600

    docs: add commenting in sync error handler branch

--- index.js
+++ index.js
@@ -7,10 +7,30 @@ app.get('/', (req, res) => {
 
 
 // note: router not found 
+// express default error handler do not handle this error 
+// because it is not server side error
+// it is a error caused by user wrong input
+
 app.use((req, res, next) => {
+  // docs: 
+  // we can handle this error from here by res.send
+
+  // res.statue(404).send('no route found')
+
+  // but if we want to send this error to the error middleware then do this 🔽🔽
   next('request url was not found!!');
 })
 
+// normal use case
+// app.use((err, req, res, next) => {
+//   // docs:all error related things will be found in the error obj 
+//   if (err.message) {
+//     res.send(err.message);
+//   } else {
+
+//       res.status(500).send('there was an error!!')
+//     }
+// })
 
 // note: error handling middleware need to be the last middleware in the file 
 // imp: 
@@ -28,4 +48,8 @@ app.use((err, req, res, next) => {
 
 })
 
+//docs: express default error handler
+// app.use((err, req, res,next)=>{
+//   // invisibley express handle error here
+// })
 app.listen(5000, () => console.log('server is running'))
