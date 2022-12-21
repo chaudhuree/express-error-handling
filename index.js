60736821da49180738e0ee012237ce4418bcaad4
chaudhuree <chaudhuree00@gmail.com>
2022-12-21 22:54:49 +0600

    docs: edit in async branch

--- index.js
+++ index.js
@@ -5,15 +5,22 @@ const {readFile} = require('fs')
 app.get('/', (req, res,next) => {
   readFile('/file-not-exist','utf-8',(err,data)=>{
     if(err){
-      next(err);
+
+      next(err); //imp: ref:1 
     }else{
       res.send(data)
     }
   })
 })
 
-
-
+// docs: 
+// if next has error parameter like ref:1 
+// then this middleware will not becalled
+// it will directly go to the error handler
+app.use((req, res, next) => {
+  console.log('i am not called');
+  next()
+})
 
 
 // note: error handling middleware need to be the last middleware in the file 
